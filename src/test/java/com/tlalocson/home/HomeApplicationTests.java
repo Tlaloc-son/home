b3d3c70cb78094b7ef7effdb60dc87e7500647ca
Tlaloc-son <isaacvchades@hotmail.com>
2018-08-20 15:40:27 -0500

    Ajuste para integracion con Jenkins

---
+++ src/test/java/com/tlalocson/home/HomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlalocson.home;
+
+
+
+//@RunWith(SpringRunner.class)
+public class HomeApplicationTests {
+
+//	@Test
+//	public void contextLoads() {
+//		assertTrue(true);
+//	}
+
+}
